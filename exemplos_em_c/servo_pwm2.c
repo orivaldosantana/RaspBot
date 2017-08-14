42f0d4af784f8a82a120dda7dd90fc4c46b7a578
orivaldosantana <orivaldo@gmail.com>
2017-08-14 03:24:01 +0000

    atualizando

--- exemplos_em_c/servo_pwm2.c
+++ exemplos_em_c/servo_pwm2.c
@@ -11,32 +11,29 @@
 
  
 int main(void) {
-        if(wiringPiSetup() == -1) { //Quando a inicialização do wiringPi falha 
-                // uma mensagem é apresentada
-                printf("Setup do wiringPi falhou!\n");
-                return -1;
-        }
-        int minValue = 23; 
-	int v = minValue; 
-	int maxValue = 123; 
-        softPwmCreate(ServoPin,minValue,maxValue);
-        pinMode(ServoPin, PWM_OUTPUT);
+	if(wiringPiSetup() == -1) { //Quando a inicialização do wiringPi falha 
+			// uma mensagem é apresentada
+			printf("Setup do wiringPi falhou!\n");
+			return -1;
+	}
+    int minValue = 25; 
+	int v = minValue;
+ 	int pwmRange= 125;
+	int maxValue = pwmRange; 
+    softPwmCreate(ServoPin,minValue,pwmRange);
+	pinMode(ServoPin, PWM_OUTPUT);
 	pwmSetMode(PWM_MODE_MS); 
 	pwmSetClock(384); //clock at 50kHz (20us tick)
-	//pwmSetRange(1024); //range at 1000 ticks (20ms)
-
-        
-        while(1) {
-		
-                if (v > maxValue) {
-					v = minValue; 
-				}			
-                pwmWrite(1, v);    
-                printf("V %i\n",v);
-                delay(1000); 
-                v += 5;  
-
-                      
-        }
-        return 0;
+	 
+    
+	while(1) {
+		if (v > maxValue) {
+			v = minValue; 
+		}			
+		pwmWrite(1, v);    
+		printf("V %i\n",v);
+		delay(1000); 
+		v += 5;  
+	}
+	return 0;
 }
